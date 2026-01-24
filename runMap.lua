6d9de8c38a5994e23ca75ba21ae69a617e19e6e6
Narrr13 <titerris13@gmail.com>
2026-01-24 14:04:18 +0100

    Update runMap.lua

--- runMap.lua
+++ runMap.lua
@@ -9,49 +9,49 @@ local map60_558 = {
     actions = {
         -- Coffre 1
         { pos = Vector3(-0.40,0.19,339.54), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 1st chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-18.52,0.09,321.60), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 2nd chamber opens." ,wait=1 },
         -- Path to coffer 2
         { pos = Vector3(-3.54,-7.52,269.17) },    
         -- Coffre 2
         { pos = Vector3(0.04,-7.8,214.89), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 2nd chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-18.23,-7.8,196.82), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 3rd chamber opens." ,wait=1 },   
         -- Path to coffer 3    
         { pos = Vector3(-3.32,-15.52,144.47) },       
         -- Coffre3
         { pos = Vector3(0.26,-15.80,90.19), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 3rd chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left    
         { pos = Vector3(-18.41,-15.90,71.42), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 4th chamber opens." ,wait=1 },      
         -- Path to coffer 4    
         { pos = Vector3(-3.571,-23.45,19.36) },
         -- Coffre4
         { pos = Vector3(0.009,-23.73,-34.92), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 4th chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-19.98,-23.8,-55.15), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 5th chamber opens." ,wait=1 },       
         -- Path to coffer 5
         { pos = Vector3(-4.37 , -31.46 , -105.58) },    
         -- Coffre5
         { pos = Vector3(-0.79,-31.74,-159.86), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 5th chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-20.15,-31.90,-179.38), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 6th chamber opens." ,wait=1 },       
         -- Path to coffer 6
         { pos = Vector3(-4.18 , -39.49 , -229.39) },    
         -- Coffre6
         { pos = Vector3(-0.60,-39.77,-283.67), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 6th chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-18.67,-39.89,-303.83), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the final chamber opens." ,wait=1 },       
         -- Path to coffer 7
         { pos = Vector3(-3.80 , -47.50 , -353.90) },    
         -- Coffre7
         { pos = Vector3(-0.22,-47.78,-408.18), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The 7th chamber is no longer sealed!", wait=1 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Exit
         { pos = Vector3(-0.21,-47.97,-424.38), action = { cmd="interact", arg={true, "door"} } }      
     }
@@ -66,34 +66,35 @@ local map70_794 = {
         { pos = Vector3(99.9,0.10,107.55), wait=2 },
         { action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The summon is dispelled."},
         { pos = Vector3(100.08,0.10,99.97), action = { cmd="interact", arg={true, "treasure"} }, wait=7 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         { pos = Vector3(100.08,0.10,99.97), wait=2 },
         -- Summon 2
         { pos = Vector3(100.10,0,110.60), wait=3 },
         { action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The summon is dispelled."},
         { pos = Vector3(100.08,0.10,99.97), action = { cmd="interact", arg={true, "treasure"} }, wait=7 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         { pos = Vector3(100.08,0.10,99.97), wait=2 },
         -- Summon 3
         { pos = Vector3(100.10,0,110.60), wait=3 },
         { action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The summon is dispelled."},
         { pos = Vector3(100.08,0.10,99.97), action = { cmd="interact", arg={true, "treasure"} }, wait=7 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         { pos = Vector3(100.08,0.10,99.97), wait=2 },
         -- Summon 4
         { pos = Vector3(100.10,0,110.60), wait=3 },
         { action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The summon is dispelled."},
         { pos = Vector3(100.08,0.10,99.97), action = { cmd="interact", arg={true, "treasure"} }, wait=7 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         { pos = Vector3(100.08,0.10,99.97), wait=2 },
         -- Summon 5
         { pos = Vector3(100.10,0,110.60), wait=3 },
         { action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The summon is dispelled."},  
         { pos = Vector3(100.08,0.10,99.97), action = { cmd="interact", arg={true, "treasure"} }, wait=7 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         { pos = Vector3(100.24,0,83.49), action = { cmd="interact", arg={true, "door"} }, wait=2 }
     }
-}        
+}
+
         
 local map70_712 = {
     zoneId = 712,
@@ -104,7 +105,7 @@ local map70_712 = {
         -- Coffre 1
         { pos = Vector3(0.082,100.39,299.91), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The First Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(0.082,100.39,299.91), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-20.18,100.00,279.70), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 2nd chamber opens." ,wait=7 },
         -- Path to coffer 2
@@ -112,7 +113,7 @@ local map70_712 = {
         -- Coffre 2
         { pos = Vector3(-0.098,50.4,179.63), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The Second Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(-0.098,50.4,179.63), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { zoneId = 712, pos = Vector3(-20.19,50.00,159.78), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 3rd chamber opens." ,wait=7 },
         -- Path to coffer 3    
@@ -120,7 +121,7 @@ local map70_712 = {
         -- Coffre3
         { pos = Vector3(0.02,0.40,60.21), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The Third Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(0.02,0.40,60.21), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left    
         { zoneId = 712, pos = Vector3(-20.20,0.00,39.73), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 4th chamber opens." ,wait=7 },   
         -- Path to coffer 4    
@@ -128,31 +129,32 @@ local map70_712 = {
         -- Coffre4
         { pos = Vector3(0.04,-49.59,-60.14), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The Fourth Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(0.04,-49.59,-60.14), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-19.75,-49.89,-79.70), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 5th chamber opens." ,wait=7 },
         -- Path to coffer 5
-        { action = { cmd="movealittle", arg={} }, wait=5} ,
+        { action = { cmd="movealittle", arg={} }, wait=5},
         -- Coffre5
         { pos = Vector3(-0.45,-99.59,-179.02), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The Fifth Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(-0.45,-99.59,-179.02), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-20.40,-99.99,-200.06), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the 6th chamber opens." ,wait=7 },       
         -- Path to coffer 6
         { action = { cmd="movealittle", arg={} }, wait=5},
+        -- Coffre6
         { pos = Vector3(-0.06,-149.59,-299.76), action = { cmd="interact", arg={true, "treasure"} }, checkOk = "%[(%d+):(%d+)%]The Sixth Sluice is no longer sealed!", wait=5 },
         { pos = Vector3(-0.06,-149.59,-299.76), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Door left
         { pos = Vector3(-20.45,-149.99,-320.02), action = { cmd="interact", arg={true, "door"} }, checkOk = "%[(%d+):(%d+)%]The gate to the final chamber opens." ,wait=7 },       
         -- Path to coffer 7
         { action = { cmd="movealittle", arg={} }, checkOk = "%[(%d+):(%d+)%]The Sevventh Sluice is no longer sealed!", wait=5},   
         -- Coffre7
         { pos = Vector3(-0.18,-199.60,-420.02), action = { cmd="interact", arg={true, "treasure"} }, wait=5 },
-        { action = { cmd="cleanBag", arg={true} } },
+        { action = { cmd="cleanBag", arg={} } },
         -- Exit
-        { pos = Vector3(0.08,-200.00,-436.21), action = { cmd="interact", arg={true, "door"} } }       
+        { pos = Vector3(0.08,-200.00,-436.21), action = { cmd="interact", arg={true, "door"} } }
     }
 }
 
@@ -172,11 +174,11 @@ function action.interact(type,multi)
     end
 end
 
-function action.cleanBag (multi)
+function action.cleanBag (partyMemberCount)
     -- defaut multi false
-    multi = multi or false
-
-    if multi then
+    partyMemberCount = partyMemberCount or 1
+    
+    if partyMemberCount > 1 then
         yield("/p autooff")
         Sleep(1)
         yield("/p cleanBag")
@@ -198,7 +200,7 @@ function action.cleanBag (multi)
         end 
         Sleep(1)
         yield("/p autofollow")
-    else
+    elseif partyMemberCount == 1 then
         for i = 0, Svc.Objects.Length - 1 do
             local obj = Svc.Objects[i]
             if obj then
