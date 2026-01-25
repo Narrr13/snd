7b4f5aeff9c5d8129d6cbd2af190e2c736a6f0c9
Narrr13 <titerris13@gmail.com>
2026-01-25 22:38:24 +0100

    Create initCombat.lua

---
+++ initCombat.lua
@@ -0,0 +1,26 @@
+require "NonuLualib"
+
+if Player.Entity.Name == "Nar W'ia" then
+    yield("/fulf on")
+    yield("/fulf need")
+    yield("/gearset change 30")
+elseif Player.Entity.Name == "Marcel Patoulachi" then
+    yield("/fulf on")
+    yield("/fulf pass")
+    yield("/gearset change 23" )
+elseif Player.Entity.Name == "Jeanpierre Vidol" then
+    yield("/fulf on")
+    yield("/fulf pass")
+    yield("/gearset change 1" )
+elseif Player.Entity.Name == "Robert Robichet" then
+    yield("/fulf on")
+    yield("/fulf pass")
+    yield("/gearset change 13" )
+end
+
+
+Sleep(1)
+yield("/rotation auto big")
+yield("/bmrai on")
+
+yield("/p Ready")
