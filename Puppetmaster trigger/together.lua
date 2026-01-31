184e16321c27749438a2b7321169fd1d2fa38dff
Narrr13 <titerris13@gmail.com>
2026-01-31 18:02:17 +0100

    update

--- Puppetmaster trigger/together.lua
+++ Puppetmaster trigger/together.lua
@@ -3,11 +3,11 @@ require("NonuLuaLib")
 if Svc.Party.Length ~= 0 then
     local partyLeaderName=Svc.Party[Svc.Party.PartyLeaderIndex].Name.TextValue
     
-    while Entity.Target.Name ~= partyLeaderName do
+    while Entity.Target==nil or Entity.Target.Name~=partyLeaderName do
         yield("/target "..partyLeaderName)
         Sleep(0.5)
     end
-    
+
     if Movement(Entity.Target.Position.X,Entity.Target.Position.Y,Entity.Target.Position.Z,false,1) then 
         yield("/p movedone")
     end
