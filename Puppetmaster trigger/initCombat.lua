8326e072397b5cc3092a1eb1c240d0a3d1437fea
Narrr13 <titerris13@gmail.com>
2026-02-20 09:46:10 +0100

    Create initCombat.lua

---
+++ Puppetmaster trigger/initCombat.lua
@@ -0,0 +1,35 @@
+--[=====[
+[[SND Metadata]]
+author: Nar
+version: 0.0.1
+description: "Prepare maps"
+
+configs:
+  loot:
+    default: "pass"
+    description: Loot pass - need
+  gearset:
+    default: 0
+    description : GearsSet
+[[End Metadata]]
+--]=====]
+
+
+
+
+require "NonuLualib"
+
+local loot = Config.Get("loot")
+local gearset = Config.Get("gearset")
+
+yield("/fulf on")
+yield("/fulf "..loot)
+yield("/gearset change "..gearset)
+Sleep(1)
+yield("/rotation auto big")
+yield("/bmrai on")
+
+yield("/li Spriggan")
+WaitForLifestream()
+yield("/li Limsa Lominsa")
+WaitForLifestream()
