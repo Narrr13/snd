0df3910d7e3ee89d9cccce56fa804556c757d3a9
Narrr13 <titerris13@gmail.com>
2026-02-23 18:24:05 +0100

    Update InteractTreasure.lua

--- Puppetmaster trigger/InteractTreasure.lua
+++ Puppetmaster trigger/InteractTreasure.lua
@@ -2,12 +2,13 @@ require("NonuLuaLib")
 
 function main()
     LogInfo("[Trigger] InteractTreasure")
-    PathToObject("trea",false,2)
+    PathToObject("trea",false,3)
     yield('/target trea')
     yield('/interact')
     Sleep(1)
     yield('/p intertdone')
 end
+
 
 
 main()
